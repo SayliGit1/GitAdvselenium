4240137aa34c5951e4dc7008364374f1c69d9a51
SayliGit1 <sayliharne4@gmail.com>
2025-03-08 15:29:01 +0530

    Update EmptyBrowser.java

--- AdvanceSeleniumFW/src/test/java/practice/EmptyBrowser.java
+++ AdvanceSeleniumFW/src/test/java/practice/EmptyBrowser.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class EmptyBrowser {
-
+//pulling from github
 	public static void main(String[] args) {
 
 		WebDriver driver = new ChromeDriver();
